526210610bfe37eb5c083be92817ac81aa8656a7
Allen B <pingguo.ny+git@gmail.com>
2017-08-10 17:05:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function addCard(title, text, link) {
   const card = document.createElement("div");
   card.classList.add("mdl-card", "mdl-shadow--2dp");
   
-  const title = document.createElement("div");
-  title.classList.add("mdl-card__title");
+  const title_e = document.createElement("div");
+  title_e.classList.add("mdl-card__title");
   
   const title_text = document.createElement("h2");
   title_text.innerHTML = title;
